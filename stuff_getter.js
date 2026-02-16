0e43acf40ad97761896943dc0162a5d01d3c3117
Maker-Gitsune <jeaniusethan@gmail.com>
2026-02-15 21:14:52 -0500

    change var in js file.

--- stuff_getter.js
+++ stuff_getter.js
@@ -1,6 +1,6 @@
 function test () {
     require('dotenv').config()
-    const sec = String(process.env.message)
+    const sec = String(process.env.key)
     //document.getElementById('dynamic')
     //    .innerHTML = sec
     //return String(process.env.message)
